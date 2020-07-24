28f4bf3eef9189367233a65b60a32ff0ad5755be
Danil <danil.lazarev.90@mail.ru>
2020-07-24 15:34:44 +0300

    Начало

---
+++ Assets/DialogueItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogItem", menuName = "Create New DialogItem")]
+public class DialogueItem : ScriptableObject
+{
+    public string NameCharacter;
+    public string text;
+    public Sprite Bckground;
+    public DialogueItem Back;
+    public DialogueItem Next;
+    public Question[] questions;
+}
+[System.Serializable]
+public class Question
+{
+    public string text;
+    public DialogueItem question;
+}
---
+++ Assets/DialogueItemManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueItemManager : MonoBehaviour
+{
+    public DialogueItem answerNode;
+    public Text TextQuestion;
+    public Text NameCharacter;
+    public GameObject Continue;
+    public GameObject Back;
+    public Image imagecontaner;
+    public GUISkin skin;
+
+    void Start()
+    {
+        UpdateUi();
+    }
+    void OnGUI()
+    {
+        GUIStyle a = skin.button;
+        if (answerNode.questions.Length != 0)
+        {
+            for (int i = 0; i < answerNode.questions.Length; i++)
+            {
+                if (GUI.Button(new Rect(Screen.width/2- Screen.width / 4, 600 + i * 50, Screen.width / 2, 50), answerNode.questions[i].text,a))
+                {
+                    answerNode = answerNode.questions[i].question;
+                    UpdateUi();
+                }
+            }
+        }
+    }
+    public void UpdateUi()
+    {
+        StartCoroutine(GetChar(answerNode.text));
+        imagecontaner.sprite = answerNode.Bckground;
+        if (answerNode.Next != null)
+        {
+            Continue.SetActive(true);
+            NameCharacter.text = answerNode.NameCharacter;
+            if (answerNode.Back != null)
+            {
+                Back.SetActive(true);
+            }
+            else Back.SetActive(false);
+        }
+        else if (answerNode.Back != null)
+        {
+            NameCharacter.text = answerNode.NameCharacter;
+            Continue.SetActive(false);
+            Back.SetActive(true);
+        }
+        else
+        {
+            Continue.SetActive(false);
+            Back.SetActive(false);
+        }
+    }
+
+    IEnumerator GetChar(string s)
+    {
+        TextQuestion.text = "";
+        foreach (var c in s)
+        {
+            TextQuestion.text += c;
+            yield return null;
+        }
+    }
+
+    public void OnClickContinue()
+    {
+        answerNode = answerNode.Next;
+        UpdateUi();
+    }
+
+    public void OnClickBack()
+    {
+        answerNode = answerNode.Back;
+        UpdateUi();
+    }
+}
